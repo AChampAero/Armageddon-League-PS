3881928437bcff01b1e1e65753603c7f894cf149
AChampAero <torresgerardo9535@gmail.com>
2014-01-13 17:28:19 -0600

    Update commands.js

--- commands.js
+++ commands.js
@@ -1357,13 +1357,16 @@ var commands = exports.commands = {
 			'Enjoy your stay!.<br /><br />' +
 			'</div>');
 		}
-		if (target.toLowerCase() == "leagueinc") {
-			return connection.sendTo('lobby', '|html|<div class = "infobox"><center><a href="http://league-inc.weebly.com//"><img src="http://league-inc.weebly.com/uploads/2/1/9/6/21965158/1385953461.png></a><center><br />' +
+                if (target.toLowerCase() == "leagueinc") {
+			return connection.sendTo('leagueinc', '|html|<div class = "infobox"><center><a href="http://league-inc.weebly.com//"><img src="http://league-inc.weebly.com/uploads/2/1/9/6/21965158/1385953461.png"></a><center><br />' +
 			'<center><b><u>Welcome To The LeagueInc!</u></b></center><br />'+
-			'Can you defeat the League?.!<br /><br />'+ 
-                        'Fight several Gym Leaders, battle the Elite Four, and face up against the Leagues Champion. Do you have what it takes?!<br /><br />'+ 
-                        '</div>');
-	},
+			'Fight several Gym Leaders, battle the Elite Four, and face up against the League Champion.!<br /><br />'+ 
+			'Do you have what it takes? <br /><br />' +
+			'Enjoy your stay!.<br /><br />' +
+			'</div>');
+
+                        }
+                }
 
 	rb: 'roomban',
 	roomban: function(target, room, user, connection) {
